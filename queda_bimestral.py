7d79ce60d6e2d2ba752ab4137ef89c779ba31b57
Jefferson <dev.jeffersonsued@gmail.com>
2025-07-23 18:35:56 -0300

    It calculates the average grade for each bimester (every 2 months), displays these averages, and then checks if there was any drop in performance.

---
+++ queda_bimestral.py
@@ -0,0 +1,20 @@
+meses = input().split(',')
+bimestres = []
+
+for i in range(0,12,2):
+    
+    media = ((int(meses[i]) + int(meses[i+1])) / 2)
+    bimestres.append(media)
+    print(f'{(i/2)+1:.0f} bimestre: {media:.1f}')
+
+print('queda bimestral:')
+
+queda = False
+
+for medias in range(1,6):
+    
+    if int(bimestres[medias]) < int(bimestres[medias - 1]):
+        print(medias + 1)
+        queda = True
+if queda == False:
+    print('nenhum')    
