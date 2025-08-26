016b4d83109ce2e2023cd3251abcc8fae38d1386
jeffsued <dev.jeffersonsued@gmail.com>
2025-08-25 23:35:22 -0300

    Add insertion sort implementation

---
+++ sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j+1], lista[j] = lista[j], lista[j+1]
+    return lista
+
+def main():
+    lista = [5, 2, 20, 70, 1, 3, 7]
+    ordenada = insertion_sort(lista)
+    print(ordenada)
+if __name__ == "__main__":    
+    main()
